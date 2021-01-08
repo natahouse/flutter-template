7983eba17a58f500f4be7ebc8f73e52e1a7a81c0
Higor Alves <higorhaalves@gmail.com>
2021-01-08 14:29:49 -0300

    Initial project

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nata House',
+      home: Scaffold(
+        body: Text('nata.house template'),
+      ),
+    );
+  }
+}
